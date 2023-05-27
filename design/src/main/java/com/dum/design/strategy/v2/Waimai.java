7714fae970b9630c5a8cd5e09f63fee3504c6b7f
FuchengDum <1727507392@qq.com>
2023-05-27 23:11:24 +0800

    添加优化有的策略模式，结合spring

---
+++ design/src/main/java/com/dum/design/strategy/v2/Waimai.java
@@ -0,0 +1,24 @@
+package com.dum.design.strategy.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import com.dum.design.strategy.WaimaiService;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+@Service
+@Slf4j
+public class Waimai extends AbstractStrategy implements Strategy{
+    @Resource
+    private  WaimaiService waimaiService;
+    @PostConstruct
+    private void init(){
+        register();
+    }
+    @Override
+    public void issue() {
+        log.info("Waimai Strategy issue");
+        waimaiService.issueWaimai();
+    }
+}
