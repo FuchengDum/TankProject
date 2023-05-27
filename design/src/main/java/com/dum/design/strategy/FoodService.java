e856694c8428812b358af3b419fa83fa7fa733a0
FuchengDum <1727507392@qq.com>
2023-05-27 23:13:10 +0800

    添加优化有的策略模式，结合spring

---
+++ design/src/main/java/com/dum/design/strategy/FoodService.java
@@ -0,0 +1,12 @@
+package com.dum.design.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class FoodService {
+    public void getCoupon(){
+        log.error("Food Service getCoupon...");
+    }
+}
