9c2cabd9eca967389477fd9c08307d49993f0f84
FuchengDum <1727507392@qq.com>
2023-05-28 13:01:37 +0800

    添加任务模型的设计的问题

--- design/src/main/java/com/dum/design/observer/ActivityService.java
+++ design/src/main/java/com/dum/design/observer/ActivityService.java
@@ -1,6 +1,10 @@
 package com.dum.design.observer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ActivityService {
     public void notifyFinished(Long taskId) {
+        log.info("ActivityService notifyFinished taskId={}",taskId);
     }
 }
--- design/src/main/java/com/dum/design/observer/TaskManager.java
+++ design/src/main/java/com/dum/design/observer/TaskManager.java
@@ -1,6 +1,10 @@
 package com.dum.design.observer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TaskManager {
     public void release(Long taskId) {
+        log.info("TaskManager release taskId={}",taskId);
     }
 }
